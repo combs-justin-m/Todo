be83136f6cfebceee6c874bef50242cfae729d4e
Justin Combs <combs.justin.m@gmail.com>
2015-06-05 18:16:37 -0400

    handlebars additions

--- app/js/main.js
+++ app/js/main.js
@@ -2,46 +2,41 @@
 (function (){
   'use strict';
 
-  var Todo = function(task){
-    // var args = options || {};
+  var Todo = function(options){
+    var args = options || {};
 
-    this.task = task;
+    this.task = args.task;
     this.status = 'Active';
   };
 
-  var storageBin = {
-    'data': ''
-  };
   var storage = [];
   var $todoForm = $('#todoForm');
   var $todoInput = $('todoInput');
-  var $listContainer = $('.listContainer');
-
-  storageBin.data = storage;
+  var $list = $('#list');
 
   $todoForm.on('submit', function(e){
     e.preventDefault();
     var taskText = $('#todoInput').val();
-    var taskInstance = new Todo(taskText);
+    var taskInstance = new Todo({task:taskText});
 
     storage.push(taskInstance);
 
     this.reset();
 
-    $listContainer.html(template.todo(storageBin));
+    $list.html(template.todo({data:storage}));
 
   });
 
-  // $list.on('click', 'li', function (e){
-  //   e.preventDefault();
+  $list.on('click', 'li', function (e){
+    e.preventDefault();
 
-  //   $(this).addClass('complete');
+    $(this).addClass('complete');
 
-  //   var currentTask = $(this).text();
-  //   var taskToEdit = _.find(storage, {task: currentTask});
+    var currentTask = $(this).text();
+    var taskToEdit = _.find(storage, {task: currentTask});
 
-  //   taskToEdit.status = 'Closed';
+    taskToEdit.status = 'Closed';
 
-  // });
+  });
 
 }());
--- app/js/templates.js
+++ app/js/templates.js
@@ -4,11 +4,10 @@ this["template"]["todo"] = Handlebars.template({"1":function(depth0,helpers,part
 
   return "    <li>"
     + this.escapeExpression(((helper = (helper = helpers.task || (depth0 != null ? depth0.task : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"task","hash":{},"data":data}) : helper)))
-    + "</li>\n\n";
+    + "</li>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return "\n\n<ul> list\n"
-    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.data : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "</ul>";
+  return "\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.data : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
